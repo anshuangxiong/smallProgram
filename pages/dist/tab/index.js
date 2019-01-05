7b6362e13dd680e10fe623ef591fa3110c9726c6
anshuangxiong <13821752883@163.com>
2019-01-05 22:09:08 +0800

    更新组件库

--- pages/dist/tab/index.js
+++ pages/dist/tab/index.js
@@ -6,21 +6,26 @@ VantComponent({
   },
   props: {
     title: String,
-    disabled: Boolean
+    disabled: Boolean,
+    dot: Boolean,
+    info: null,
+    titleStyle: String
   },
   data: {
     inited: false,
-    active: false
+    active: false,
+    animated: false,
+    width: null
   },
   watch: {
-    disabled: function disabled() {
-      var parent = this.getRelationNodes('../tabs/index')[0];
-
-      if (parent) {
-        parent.updateTabs();
-      }
-    },
-    title: function title() {
+    title: 'update',
+    disabled: 'update',
+    dot: 'update',
+    info: 'update',
+    titleStyle: 'update'
+  },
+  methods: {
+    update: function update() {
       var parent = this.getRelationNodes('../tabs/index')[0];
 
       if (parent) {
