f31c85ee5d22d5064a35b07c8390fe075a2d4a64
anshuangxiong <13821752883@163.com>
2019-01-06 15:57:52 +0800

    我的页面

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log("登录成功")
+        console.log(res);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log("获取用户信息成功")
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log("获取用户信息成功2")
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
@@ -29,6 +33,8 @@ App({
               }
             }
           })
+        }else{
+          console.log("没有授权else");
         }
       }
     })
