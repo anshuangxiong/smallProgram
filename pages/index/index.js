94afddacffc3ec50e1061c365c06c216c59a1c30
anshuangxiong <13821752883@163.com>
2019-01-06 13:21:26 +0800

    更新底部tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,101 +1,128 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    phone: 'anx',
-    password: '123456'
+    active:0,
+    activeNames: '1',
+    noactive:-1,
+    fixed:true,
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  tap() {
+    console.log('tap')
   },
-
-  // 获取输入账号 
-   phoneInput: function (e) {
-     this.setData({
-        phone: e.detail.value
-     })
-   },
-
-  // 获取输入密码 
-   passwordInput: function (e) {
-     this.setData({
-        password: e.detail.value
-     })
-   },
-
-  // 登录 
-   login: function () {
-     if (this.data.phone.length == 0 ) {
-        wx.showToast({
-          title: '用户名不能为空',
-          icon: 'loading',
-          duration: 2000
-        })
-     }else if (this.data.password.length == 0) {
-       wx.showToast({
-         title: '密码不能为空',
-         icon: 'loading',
-         duration: 2000
-       })
-     } else {
-       // 这里登录成跳转的页面 
-       if (this.data.password == '123456'&&this.data.phone=='anx'){
-         wx.showToast({
-           title: '登录成功',
-           icon: 'success',
-           duration: 2000
-         });
-        // console.log("登录成功");
-         wx.navigateTo({
-           url: '/pages/menu/menu?userId=' + this.data.phone　　// 页面 A
-         })
-       }
-     }
-   },
-
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  onChangeOfMainMenu(event) {
+    console.log(event);
+    this.setData({
+      active: event.detail
+    });
+    switch (event.detail){
+      case 0:{
+        wx.switchTab({
+          url: '/pages/index/index' // 首页
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+      case 1:{
+
+      }
+      case 2:{
+        wx.switchTab({
+          url: '/pages/mock/mock' // 我的
+        })
+      }
+      case 3:{
+        wx.switchTab({
+          url: '/pages/my/my' // 我的
+        })
+      }
+      default:{
+        wx.switchTab({
+          url: '/pages/index/index' // 首页
+        })
+      }
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onChange(event) {
+    console.log('change');
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+      activeNames: event.detail
+    });
+  },
+  onLoad: function () {
+    //console.log("menu 载入");
+    // wx.drawCanvas({
+    //   canvasId: 'firstCanvas',
+    //   actions: context.getActions() // 获取绘图动作数组
+    // });
+    var wxCharts = require('../js/wx-charts/wxcharts.js');
+    // new wxCharts({
+    //   canvasId: 'pieCanvas',
+    //   type: 'pie',
+    //   series: [{
+    //     name: 'cat1',
+    //     data: 50,
+    //   }, {
+    //     name: 'cat2',
+    //     data: 30,
+    //   }, {
+    //     name: 'cat3',
+    //     data: 1,
+    //   }, {
+    //     name: 'cat4',
+    //     data: 1,
+    //   }, {
+    //     name: 'cat5',
+    //     data: 46,
+    //   }],
+    //   width: 400,
+    //   height: 280,
+    //   dataLabel: true
+    // });
+
+
+    // new wxCharts({
+    //   canvasId: 'lineCanvas',
+    //   type: 'line',
+    //   categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
+    //   series: [{
+    //     name: '成交量1',
+    //     data: [0.15, 0.2, 0.45, 0.37, 0.4, 0.8],
+    //     format: function (val) {
+    //       return val.toFixed(2) + '万';
+    //     }
+    //   }, {
+    //     name: '成交量2',
+    //     data: [0.30, 0.37, 0.65, 0.78, 0.69, 0.94],
+    //     format: function (val) {
+    //       return val.toFixed(2) + '万';
+    //     }
+    //   }],
+    //   yAxis: {
+    //     title: '成交金额 (万元)',
+    //     format: function (val) {
+    //       return val.toFixed(2);
+    //     },
+    //     min: 0
+    //   },
+    //   width: 360,
+    //   height: 200
+    // });
+
+    // new wxCharts({
+    //   canvasId: 'columnCanvas',
+    //   type: 'column',
+    //   categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
+    //   series: [{
+    //     name: '成交量1',
+    //     data: [15, 20, 45, 37, 4, 80]
+    //   }, {
+    //     name: '成交量2',
+    //     data: [70, 40, 65, 100, 34, 18]
+    //   }],
+    //   yAxis: {
+    //     format: function (val) {
+    //       return val + '万';
+    //     }
+    //   },
+    //   width: 360,
+    //   height: 200
+    // });
   }
 })
